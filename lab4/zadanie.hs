8041936a478a8a146c979dbd6c55f29cc1037bfe
Marek Bardoński <bdfhjk@gmail.com>
2016-03-31 11:30:48 +0200

    lab4

---
+++ lab4/zadanie.hs
@@ -0,0 +1,27 @@
+import Control.Monad
+
+inits :: [a] -> [[a]]
+inits [] = return []
+inits (x:xs) = return [] `mplus` do
+  ys <- inits xs
+  (return (x:ys))
+
+allPairs :: [a] -> [a] -> [[a]]
+allPairs xs ys = [[x,y] | x <- xs, y <- ys]
+
+allPairs2 :: [Int] -> [Int] -> [[Int]]
+allPairs2 xs ys = do
+  x <- xs
+  y <- ys
+  return [x,y]
+
+allCombinations :: [[a]] -> [[a]]
+allCombinations [] = [[]]
+allCombinations (xs:xss) = do
+  x <- xs
+  l <- allCombinations xss
+  return (x:l)
+
+main :: IO()
+main =
+  print (allCombinations [[1,2], [4,5], [6], [7]])
