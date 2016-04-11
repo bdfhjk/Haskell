324bc07668d91e76ca7d27e66f726f3df2ae3cf9
Marek Bardoński <bdfhjk@gmail.com>
2016-04-11 12:54:01 +0200

    monad_state

--- lab5/monad_state.hs
+++ lab5/monad_state.hs
@@ -1,17 +1,30 @@
 import Control.Monad.State
 
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Eq, Ord, Show)
-type S a = Int -> a
+type S a = State Int a
 
-renumberTree :: Tree a -> S Int -> Tree Int
+renumberTree :: Tree a -> S (Tree Int)
 main :: IO()
 
+insert :: (Ord a) => a -> Tree a -> Tree a
+insert a Empty = Node a Empty Empty
+insert a (Node b l r) = Node b l2 r2 where
+    l2 = if a < b then insert a l else l
+    r2 = if a > b then insert a r else r
+
+fromList :: (Ord a) => [a] -> Tree a
+fromList = foldr insert Empty
+
+toList :: Tree a -> [a]
+toList Empty = []
+toList (Node a l r) = toList l ++ [a] ++ toList r
 
 renumberTree Empty = return Empty
 renumberTree (Node _ tl tr) = do
+  tl' <- renumberTree tl
   d <- get
-  tl' <- put (d+1) (renumberTree tl)
-  tr' <- put (d+1) (renumberTree tr)
+  put (d+1)
+  tr' <- renumberTree tr
   return (Node d tl' tr')
 
-main = print $ (renumberTree Empty 0 )
+main = print $ toList $ evalState (renumberTree $ fromList "Learn Haskell") 0
