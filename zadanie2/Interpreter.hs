fc4e5219f548bba00b463a962132e55a24abdcea
Marek Bardoński <bdfhjk@gmail.com>
2016-04-19 12:22:30 +0200

    .

--- zadanie2/Interpreter.hs
+++ zadanie2/Interpreter.hs
@@ -2,51 +2,61 @@ module Main where
 
 import System.IO ( stdin, hGetContents )
 import System.Environment ( getArgs, getProgName )
-
 import Parsyntax
 import Lexsyntax
 import Abssyntax
 import Skelsyntax
 import Printsyntax
-
-
 import ErrM
 
 type ParseFun a = [Token] -> Err a
-
-showTree :: (Show a, Print a) => Int -> a -> IO ()
-showTree v tree
- = do
-      putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
-      putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
-
-
 type Verbosity = Int
+type Env = String -> Exp
 
-putStrV :: Verbosity -> String -> IO ()
-runFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
-run     :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
+envLookup s   env = env s
+envBind   s v env = (\s' -> if s == s' then v else env s) 
+emptyEnv = error "Not found"
+
+showTree    :: (Show a, Print a) => Int -> a -> IO ()
+putStrV     :: Verbosity -> String -> IO ()
+processFile :: Verbosity -> ParseFun Exp -> FilePath -> IO ()
+process     :: Verbosity -> ParseFun Exp -> String -> IO ()
+main        :: IO ()
+
+eval :: Exp -> Env -> Integer
+run  :: Exp -> Integer
+
+eval (EInt v) _ = v
+eval (EAdd e1 e2) env = (eval e1 env) + (eval e2 env)
+
+run e  = eval e emptyEnv
+
+
+showTree v tree = do
+    putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
+    putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
 
 putStrV v s = if v > 1 then putStrLn s else return ()
-runFile v p f = readFile f >>= run v p
 
-run v p s = let ts = myLexer s in case p ts of
-           Bad s    -> do putStrLn "\nParse              Failed...\n"
+processFile v p f = readFile f >>= process v p
+
+process v p s = let ts = myLexer s in case p ts of
+           Bad s    -> do putStrLn "Parse              Failed...\n"
                           putStrV v "Tokens:"
                           putStrV v $ show ts
                           putStrLn s
-           Ok  tree -> do putStrLn "\nParse Successful!"
+           Ok  tree -> do putStrV v "Parse Successful!"
                           showTree v tree
+                          putStrLn $ show (run tree)
 
 
 
-main :: IO ()
 main = do args <- getArgs
           case args of
-            [] -> hGetContents stdin >>= run 0 pExp
+            [] -> hGetContents stdin >>= process 0 pExp
             "-v":fs -> case fs of 
-                fss:[] -> runFile 2 pExp fss
-                fss -> putStrLn "Incorrect arguments"
-            fs:[] -> runFile 0 pExp fs
-            fs -> putStrLn "Incorrect arguments"
+                fss:[] -> processFile 2 pExp fss
+                fss    -> putStrLn "Incorrect arguments"
+            fs:[]      -> processFile 0 pExp fs
+            fs         -> putStrLn "Incorrect arguments"
 
